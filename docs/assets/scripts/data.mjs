987790a47a1cf54defd1ee77ac42914280722fa6
Tobi Balogun <tobi.balo@gmail.com>
2022-07-23 12:32:07 +0100

    add months of the year to data.mjs

--- docs/assets/scripts/data.mjs
+++ docs/assets/scripts/data.mjs
@@ -1,10 +1,19 @@
 // Network operator object
+const networkOperators = {
+  MTN: ["0702", "0703", "0706", "0803", "0806", "0810", "0813", "0814", "0816", "0903", "0906"],
+  AIRTEL: ["0701", "0708", "0802", "0808", "0812", "0902", "0907", "0901"],
+  '9mobile': ["0809", "0817", "0818", "0908", "0909"],
+  GLO: ["0705", "0805", "0807", "0811", "0815", "0905"],
+  NTEL: ["0804"],
+};
+
+// Months of the year
+const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 export function getNetworkOperators() {
-  return {
-    MTN: ["0702", "0703", "0706", "0803", "0806", "0810", "0813", "0814", "0816", "0903", "0906"],
-    AIRTEL: ["0701", "0708", "0802", "0808", "0812", "0902", "0907", "0901"],
-    '9mobile': ["0809", "0817", "0818", "0908", "0909"],
-    GLO: ["0705", "0805", "0807", "0811", "0815", "0905"],
-    NTEL: ["0804"],
-  };
+  return networkOperators;
+}
+
+export function getMonthsOfTheYear() {
+  return monthsOfYear;
 }
